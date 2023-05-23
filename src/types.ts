0ffb1b0809cf58f7cb7c95125b991a1ade96c317
laktam <laktamlaktamabdellah@gmail.com>
2023-05-23 11:01:58 +0100

    add types file

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+export type User = {
+    name: string;
+    password: string;
+    email: string;
+}
+
+export type Profile = {
+    username: string;
+    sub: number;
+    iat: number;
+    exp: number;
+}
+
+export type LoginUser = {
+    name: string;
+    password: string;
+}
+
+export type ProductDB = {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+    cart: {
+        id: number;
+        quantity: number;
+    }
+}
+
+export type OrderDB = {
+    id: number;
+    product: ProductDB;
+    quantity: number;
+}
+
+export type Card = {
+    name: string;
+    number: string;
+    expirationDate: string;
+    cvv: string;
+}
